be19d60c044d0c8ce20c6af957e9ec9d61b0e33e
Mike Bostock <mbostock@gmail.com>
2018-05-10 15:42:12 -0700

    Adopt rollup-plugin-uglify.

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,33 @@
-export default {
-  input: "index",
-  extend: true,
-  output: {
-    format: "umd",
-    name: "d3"
+import uglify from "rollup-plugin-uglify";
+import meta from "./package.json";
+
+const copyright = `// ${meta.name} Version ${meta.version} Copyright ${new Date().getFullYear()} Observable, Inc.`;
+
+export default [
+  {
+    input: "index",
+    output: {
+      extend: true,
+      file: "dist/d3-require.js",
+      banner: copyright,
+      format: "umd",
+      name: "d3"
+    }
+  },
+  {
+    input: "index",
+    plugins: [
+      uglify({
+        output: {
+          preamble: copyright
+        }
+      })
+    ],
+    output: {
+      extend: true,
+      file: "dist/d3-require.min.js",
+      format: "umd",
+      name: "d3"
+    }
   }
-};
+];
